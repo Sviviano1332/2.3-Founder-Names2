1a24bdeedb7edd3c50e4f9731f9d7fe4d3e80fba
Sophia Viviano <sviviano@zagmail.gonzaga.edu>
2019-09-03 11:52:46 -0700

    2.4 Founders Project

--- 2.3 Founder Names/ViewController.swift
+++ 2.3 Founder Names/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var founder1: UILabel!
+    @IBOutlet weak var founder2: UILabel!
+    
+    var founders = ["Sergey Brin","Larry Page","Jennifer Hyman","Jenny Bleiss"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func bGoogle(_ sender: UIButton) {
+        print(founders[0])
+        print(founders[1])
+    }
+    @IBAction func bRunway(_ sender: UIButton) {
+        print(founders[2])
+        print(founders[3])
+            }
+    @IBAction func bClear(_ sender: UIButton) {
+       
+    }
+    
 }
 
